1e84a409ebf2ba81906340f743b8445af2816ae2
sksehgalaman11 <67869546+sksehgalaman11@users.noreply.github.com>
2020-07-06 12:19:03 +0530

    Add files via upload
    Product
     
    This defines the interface of objects the factory method creates

---
+++ IOrderType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderProcessingSystem.Models;
+
+namespace OrderProcessingSystem
+{
+    interface IOrderType
+    {
+        void  PackingSlip();
+        List<Client> globalmembers { get; set; }
+
+    }
+}
