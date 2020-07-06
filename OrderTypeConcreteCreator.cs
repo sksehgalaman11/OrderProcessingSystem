2a8009527b36228a9b7d99ee6ba67ce92430c3c3
sksehgalaman11 <67869546+sksehgalaman11@users.noreply.github.com>
2020-07-06 12:17:47 +0530

    Add files via upload
    Creator
     
    This is an abstract class and declares the factory method, which returns an object of type Product.
     
    This may also define a default implementation of the factory method that returns a default ConcreteProduct object.
    This may call the factory method to create a Product object.
    ConcreteCreator
     
    This is a class that implements the Creator class and overrides the factory method to return an instance of a ConcreteProduct.

---
+++ OrderTypeConcreteCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderProcessingSystem.Models;
+
+namespace OrderProcessingSystem
+{
+   class OrderTypeConcreteCreator : OrderTypeCreator
+    {
+        public override IOrderType  ProcessOrderType(string type)
+        {
+
+            switch (type)
+            {
+                case "PhysicalProduct": return new PhysicalProduct();
+                case "BookProduct": return new BookProduct();
+                case "MembershipProduct": return new MembershipProduct();
+                case "VideoProduct": return new VideoProduct();
+
+                default: throw new ArgumentException("Invalid type", "type");
+            }
+        }
+
+    }
+
+
+}
---
+++ OrderTypeCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderProcessingSystem.Models;
+
+namespace OrderProcessingSystem
+{
+   abstract class OrderTypeCreator
+    {
+        public abstract IOrderType ProcessOrderType(string type);
+    }
+}
