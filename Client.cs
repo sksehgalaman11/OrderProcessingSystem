f9329b8e4bc3d7343f07bb3410e1ac558b691923
sksehgalaman11 <67869546+sksehgalaman11@users.noreply.github.com>
2020-07-06 12:13:30 +0530

    Add files via upload
    Description to the solution : 
    The solution uses the Factory design pattern and solid principles to achieve the goal . 
    The unit tests have been covered to test the scenarios and given a TDD Approach .
    few values have been hard coded due to the missing part of the database within the time frame . 
    Total duration utilized : 1:30 hrs.
    The code is developed in .Net Framework Environment 4.6.1 .

---
+++ Client.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderProcessingSystem.Models
+{
+    /// <summary>
+    ///     Client model
+    /// </summary>
+    public class Client : Entity<int>
+    {
+        /// <summary>
+        ///     FirstName of client
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        ///     LastName of client
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///     Company name
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        ///     Email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        ///     Home or work phone number
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        ///     Contract date. The value can be null if no contract date exits
+        /// </summary>
+        public DateTime? MemberShipDate { get; set; }
+
+        /// <summary>
+        ///     Type of client
+        ///     possible values are : Small Business, Individual, Corporation
+        /// </summary>
+        public string ClientType { get; set; }
+
+        /// <summary>
+        ///     Extra notes
+        /// </summary>
+        public string Notes { get; set; }
+
+
+        /// <summary>
+        ///     Billing Address
+        /// </summary>
+        public virtual Address BillingAddress { get; set; }
+
+        /// <summary>
+        ///     Mailing Address
+        /// </summary>
+        public virtual Address MailingAddress { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
